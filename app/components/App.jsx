a8d2f17cdb1000c34d9ef096b8fc2cc852a4a8b0
Eric Gauderman <eric.gauderman@gmail.com>
2016-05-19 16:25:56 -0700

    setState

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,68 @@
+import uuid from 'node-uuid';
 import React from 'react';
-// Note that we can leave off the ".jsx". This also means that if there's a
-//   Note.js, that will override the Note.jsx, which is what we actually want.
-import Note from './Note';
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack',
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React',
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry right right now',
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry really later',
+        },
+      ],
+    };
   }
+
+  render() {
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <ul>{notes.map(note =>
+          <li key={note.id}>[{note.task}]</li>
+        )}</ul>
+      </div>
+    );
+  }
+
+  // We are using an experimental feature known as property
+  // initializer here. It allows us to bind the method `this`
+  // to point at our *App* instance.
+  //
+  // Alternatively we could `bind` at `constructor` using
+  // a line, such as this.addNote = this.addNote.bind(this);
+  addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New tasks'
+      }])
+    });
+  };
 }
---
+++ archive/README.txt
@@ -0,0 +1,1 @@
+This directory is for files that were used in previous commits but are no longer needed, and are kept for reference purposes
