e95a0cce1e925f344f7563a75c603b4e9d33566c
Eric Gauderman <eric.gauderman@gmail.com>
2016-05-20 13:50:05 -0700

    Demonstration and comments about the behavior of bind

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -37,10 +37,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onEdit={this.editNote}
-        />
+        <Notes notes={notes} onEdit={this.editNote}/>
       </div>
     );
   }
