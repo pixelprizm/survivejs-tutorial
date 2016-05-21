65e8e30d70537deb2d6f374ca763d19a35b03429
Eric Gauderman <eric.gauderman@gmail.com>
2016-05-20 18:11:10 -0700

    Retain state!

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,13 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import alt from './libs/alt';
 import App from './components/App.jsx';
+import persist from './libs/persist';
+import storage from './libs/storage';
+
+// Bootstrap and set up persistent app state
+persist(alt, storage, 'app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ app/libs/persist.js
@@ -0,0 +1,23 @@
+import makeFinalStore from 'alt-utils/lib/makeFinalStore';
+
+export default function(alt, storageLib, storeKey) {
+  const finalStore = makeFinalStore(alt);
+
+  try {
+    alt.bootstrap(storageLib.get(storeKey));
+  }
+  catch(e) {
+    console.error('Failed to bootstrap data', e);
+  }
+
+  // Store the application's state to localStorage whenever it changes.
+  finalStore.listen(() => {
+    // Only store the application's state if 'debug' isn't present in the store.
+    // This allows us to clear the local memory by executing the following one-liner:
+    //   localStorage.setItem('debug', 'true'); localStorage.clear();
+
+    if(!storageLib.get('debug')) {
+      storageLib.set(storeKey, alt.takeSnapshot());
+    }
+  });
+}
