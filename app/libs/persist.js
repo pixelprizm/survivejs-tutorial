f178de8b77a2eb73bc102b42d335dc915e7e6560
Eric Gauderman <eric.gauderman@gmail.com>
2016-05-22 14:18:52 -0700

    comment

--- app/libs/persist.js
+++ app/libs/persist.js
@@ -13,7 +13,8 @@ export default function(alt, storageLib, storeKey) {
   // Store the application's state to localStorage whenever it changes.
   finalStore.listen(() => {
     // Only store the application's state if 'debug' isn't present in the store.
-    // This allows us to clear the local memory by executing the following one-liner:
+    // This allows us to clear the local memory by executing the following
+    //   one-liner in the browser developer console:
     //   localStorage.setItem('debug', 'true'); localStorage.clear();
 
     if(!storageLib.get('debug')) {
